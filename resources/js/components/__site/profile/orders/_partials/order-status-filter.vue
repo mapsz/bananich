dbdf2c561543cbb31261a3d0795cafeb185d0e17
Jurij Gergelaba <jurij.gergelaba@gmail.com>
2020-09-14 09:49:24 +0300

    statuses

--- resources/js/components/__site/profile/orders/_partials/order-status-filter.vue
+++ resources/js/components/__site/profile/orders/_partials/order-status-filter.vue
@@ -3,28 +3,28 @@
     <!-- Первая вкладка (активная) -->
     <li class="nav-item">
       <a class="nav-link active" href="#all">Все</a>
-      <div class="order-num black">1500</div>
+      <!-- <div class="order-num black">1500</div> -->
     </li>
     
     <li class="nav-item">
       <a class="nav-link" href="#delivered">Доставлен</a>
-      <div class="order-num black">1500</div>
+      <!-- <div class="order-num black">1500</div> -->
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#assembly">Собирается</a>
-      <div class="order-num">1500</div>
+      <!-- <div class="order-num">1500</div> -->
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#сanceled">Отменен</a>
-      <div class="order-num">1500</div>
+      <!-- <div class="order-num">1500</div> -->
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#ready">Собран</a>
-      <div class="order-num">1500</div>
+      <!-- <div class="order-num">1500</div> -->
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#way">В пути</a>
-      <div class="order-num">150</div>
+      <!-- <div class="order-num">150</div> -->
     </li>
   </ul>
 </template>
