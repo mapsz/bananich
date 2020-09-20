ec685c5add59ae675a69d3322b8d92eced7e6994
Jurij Gergelaba <jurij.gergelaba@gmail.com>
2020-09-20 19:23:52 +0300

    path fix

--- resources/js/components/__site/products/_partials/product-gallery-card.vue
+++ resources/js/components/__site/products/_partials/product-gallery-card.vue
@@ -7,7 +7,7 @@
         :style='"background-image:url(\""+product.mainImage+"\")"'
       > 
         <a
-          :href="$route.fullPath+($route.fullPath == '/'? '':'/')+'product/'+product.id" 
+          :href="$route.path + ($route.path == '/' ? '':'/') + 'product/' + product.id" 
           style="position:absolute; width:100%; height:100%"          
         ></a>
         <!-- Bonus -->
