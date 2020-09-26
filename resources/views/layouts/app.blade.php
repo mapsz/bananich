059ef4291127ef48c9bb1be4b4e1dfec4e681b97
Jurij Gergelaba <jurij.gergelaba@gmail.com>
2020-09-26 20:04:44 +0300

    production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} local</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
