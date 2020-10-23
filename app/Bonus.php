0c13b67b2650e26fce9a767a915031c7d74f687d
Jurij Gergelaba <jurij.gergelaba@gmail.com>
2020-10-23 11:04:58 +0300

    bonus die fix

--- app/Bonus.php
+++ app/Bonus.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Log;
 use App\BonusAdd;
 use App\BonusRemove;
 use App\BonusComment;
