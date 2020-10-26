6ada78df6b9557f517f168686593659b2acfd1b9
Jurij Gergelaba <jurij.gergelaba@gmail.com>
2020-10-26 14:59:39 +0200

    email

--- app/Email.php
+++ app/Email.php
@@ -51,8 +51,10 @@ class Email extends Model
         "";
 
         $productHtml_3 = "".
-            ($product->price != $product->final_price) ? 
-            ("<span style='font-size: medium;text-decoration: line-through;'>{$product->price}</span>") : ("") .
+            (
+              ($product->price != $product->final_price) ? 
+              "<span style='font-size: medium;text-decoration: line-through;'>{$product->price}</span>" : "" 
+            ) .
             "<span style='font-size: medium;color: rgb(255, 92, 0);'><b> {$product->final_price}</b></span>".
         "";
 
