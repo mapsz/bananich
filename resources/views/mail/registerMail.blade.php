c644c4e3157a8c622fde7cc7fe4036c568a2fd10
Jurij Gergelaba <mapss@inbox.lv>
2021-01-20 18:06:09 +0300

    neo email

--- resources/views/mail/registerMail.blade.php
+++ resources/views/mail/registerMail.blade.php
@@ -6,14 +6,16 @@ $color = (isset($site) && $site == 'x') ? '#8ac2a7' : '#FBD610'
 ?>
 
   <div>
-    Спасибо, что зарегистрировались на NEO LAVKA. Для того, чтобы завершить процесс регистрации, пожалуйста, перейдите по <a href="{{$link}}">ссылке</a>
+    Спасибо, что зарегистрировались на NEO LAVKA. Для того, чтобы завершить процесс регистрации, пожалуйста, нажминтье на кнопку ниже
   </div>
   
   <br>
 
   <div style="display: flex;">
     <center>
-      <a href="{{$link}}" style="color:black; text-decoration: none; margin: auto; background-color:{{$color}};border: 0;padding: 10px;border-radius: 50px;">Кнопка</a>
+      <a href="{{$link}}" style="color:black; text-decoration: none; margin: auto; background-color:{{$color}};border: 0;padding: 10px;border-radius: 50px;">
+        Зарегистрироваться
+      </a>
     </center>
   </div>  
 
