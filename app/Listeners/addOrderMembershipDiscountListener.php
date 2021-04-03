4b300c870883ddcf51b414b03d9b40cdf4cbcc9e
Jurij Gergelaba <mapss@inbox.lv>
2021-04-03 06:49:48 +0300

    membership

--- app/Listeners/addOrderMembershipDiscountListener.php
+++ app/Listeners/addOrderMembershipDiscountListener.php
@@ -7,6 +7,8 @@ use Illuminate\Queue\InteractsWithQueue;
 
 use App\User;
 use App\Order;
+use App\Setting;
+use App\OrderMeta;
 
 class addOrderMembershipDiscountListener
 {
@@ -22,6 +24,12 @@ class addOrderMembershipDiscountListener
 
       //No membership
       if(!isset($user->memberships) || !isset($user->memberships[0])) return false;
+
+      //Check price
+      $defaultPrice = (new Setting)->getList(1)['x_order_price'];
+      $orderPrice = OrderMeta::where('name', 'participation_price')->where('order_id', $order->id)->first();
+
+      if(!$orderPrice || !$defaultPrice || !isset($orderPrice->value) || $orderPrice->value >= $defaultPrice) return;
 
       Order::addExtraCharge($order->id, $user->memberships[0]->name, -$user->memberships[0]->value);      
 
